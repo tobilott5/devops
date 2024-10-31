b0ca35174ec23b3eb209e3915da54f2ac3c4b815
tobilott5 <112110181+tobilott5@users.noreply.github.com>
2024-10-31 15:36:43 +0000

    initial files

---
+++ index.js
@@ -0,0 +1,11 @@
+exports.fibonacci = function(n) {
+    if(n==0 || n==1)
+        return n;
+    return this.fibonacci(n-1) + this.fibonacci(n-2);
+}
+
+exports.factorial = function(n) {
+    if(n==0 || n==1)
+        return 1;
+    return (n * this.factorial(n-1));
+}
